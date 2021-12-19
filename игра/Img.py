78d90460fdf7c01948c1466630e87a4cac6010cb
Stann19 <pstanislav2003@gmail.com>
2021-12-20 00:45:29 +0300

    что-то скачал

--- игра/Img.py
+++ игра/Img.py
@@ -2,11 +2,12 @@ import pygame
 
 pygame.init()
 
-bg = pygame.image.load('img/bg.jpg')
-bg2 = pygame.image.load('img/bg2.jpg')
-bg3 = pygame.image.load('img/bg3.jpg')
-bg4 = pygame.image.load('img/bg4.jpg')
-bg5 = pygame.image.load('img/bg5.jpg')
-bg6 = pygame.image.load('img/bg6.jpg')
+bckg = pygame.image.load('img/bg1.jpg')
 bg_end = pygame.image.load('img/bg_end.jpg')
 bg_menu = pygame.image.load('img/bg_menu.jpg')
+themeimage = pygame.image.load('img/choose_theme.jpg')
+levelimage = pygame.image.load('img/choose_level.jpg')
+
+def set_theme(num):
+    global bckg
+    bckg = pygame.image.load(r'img/bg{}.jpg'.format(num))
